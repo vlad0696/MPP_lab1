7f8b75173a1a2498143e34ced360eee85efb2f83
vlad0696 <vladislav-gramovich@mail.ru>
2016-09-12 23:45:20 +0300

    Добавлен Comparator

---
+++ MPP_lab1/MPP_lab1/MyComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MPP_lab1
+{
+    class MyComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x > y)
+                return 1;
+            else if (x == y)
+                return 0;
+            else
+                return -1;
+        }
+    }
+}
---
+++ MPP_lab1/QuickSortUnitTests/QSUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace QuickSortUnitTests
+{
+    [TestClass]
+    public class QSUnitTest
+    {
+       
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
